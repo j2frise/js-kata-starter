52a1a0c28bc8804c5bd982857b8a7c90b35eddd0
unknown <jarce.boukoro@korian.fr>
2022-01-26 13:36:34 +0100

    correctif paramètre d'entrée

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,40 +5,40 @@ describe("Testing fractionCalc", () => {
   it("That's a test 1!", function () {
     const fraction1Test: FractionDTO = { numerator: 1, denominator: 3 };
     const fraction2Test: FractionDTO = { numerator: 1, denominator: 2 };
-    const result = fractionCalc(fraction1Test, fraction2Test);
-    const correct = "5/6"
+    const result: string = fractionCalc(fraction1Test, fraction2Test);
+    const correct: string = "5/6"
     expect(result).toEqual(correct);
   });
 
   it("That's a test 2!", function () {
     const fraction1Test: FractionDTO = { numerator: 5, denominator: 5 };
     const fraction2Test: FractionDTO = { numerator: 5, denominator: 5 };
-    const result = fractionCalc(fraction1Test, fraction2Test);
-    const correct = "2"
+    const result: string = fractionCalc(fraction1Test, fraction2Test);
+    const correct: string = "2"
     expect(result).toEqual(correct);
   });
 
   it("That's a test 3!", function () {
     const fraction1Test: FractionDTO = { numerator: 1, denominator: 2 };
     const fraction2Test: FractionDTO = { numerator: 1, denominator: 2 };
-    const result = fractionCalc(fraction1Test, fraction2Test);
-    const correct = "1"
+    const result: string = fractionCalc(fraction1Test, fraction2Test);
+    const correct: string = "1"
     expect(result).toEqual(correct);
   });
 
   it("That's a test 4!", function () {
     const fraction1Test: FractionDTO = { numerator: 5, denominator: 3 };
     const fraction2Test: FractionDTO = { numerator: 6, denominator: 5 };
-    const result = fractionCalc(fraction1Test, fraction2Test);
-    const correct = "43/15"
+    const result: string = fractionCalc(fraction1Test, fraction2Test);
+    const correct: string = "43/15"
     expect(result).toEqual(correct);
   });
 
   it("That's a test 5!", function () {
     const fraction1Test: FractionDTO = { numerator: 1, denominator: 0 };
     const fraction2Test: FractionDTO = { numerator: 2, denominator: 5 };
-    const result = fractionCalc(fraction1Test, fraction2Test);
-    const correct = "incorrect operation, division by 0"
+    const result: string = fractionCalc(fraction1Test, fraction2Test);
+    const correct: string = "incorrect operation, division by 0"
     expect(result).toEqual(correct);
   });
 });
